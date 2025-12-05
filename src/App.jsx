1d57a4bc339969e650527c0909640e3d26bd0f31
maimuna03134 <mj9644212@gmail.com>
2025-12-06 01:12:58 +0600

    added my portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Toaster } from 'react-hot-toast';
+import './App.css'
+import MyPortfolio from './portfolio/MyPortfolio'
+
+function App() {
+
+
+  return (
+    <>
+      <MyPortfolio />
+      <Toaster />
+    </>
+  );
+}
+
+export default App
